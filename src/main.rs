59c38d53c0036bed5ac7f1c3f683ee91c77fc427
veprolet <petr.sche@seznam.cz>
2022-04-01 12:09:17 +0200

    Update clap to 3.1.7

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 extern crate clap;
-use clap::{App, Arg};
+use clap::{Command, Arg};
 
 use hash_id::{run, Config};
 
 fn main() {
-    let matches = App::new("Hash Identifier")
+    let matches = Command::new("Hash Identifier")
         .version("0.1.0")
         .author("Pedro Tashima <pedrotashima@protonmail.com>")
         .about("Identify different types of hashes")
         .arg(
-            Arg::with_name("hash")
-                .short("h") 
+            Arg::new("hash")
+                .short('h') 
                 .long("hash")
                 .value_name("STRING")
                 .help("Hash value to be identified")
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("file")
-                .short("f")
+            Arg::new("file")
+                .short('f')
                 .long("file")
                 .value_name("FILE")
                 .help("File containing hashes (each one in a line)")
